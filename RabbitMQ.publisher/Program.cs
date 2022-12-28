dfa4db9cec23c3e67b09580097acc379834b2eda
Kerem Dadak <dadakkerem@gmail.com>
2022-12-28 14:43:01 +0300

    Add project files.

---
+++ RabbitMQ.publisher/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace RabbitMQ.publisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672/");
+            using var connection= factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.QueueDeclare("hello-queue", true, false, false);
+
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+                string message = $"Message {x}";
+                var messageBody = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(String.Empty, "hello-queue", null, messageBody);
+                Console.WriteLine($"Mesaj gönderilmiştir: {message}");
+                
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMQ.subscriber/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMQ.subscriber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672/");
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+
+            channel.BasicQos(0, 1, false);
+            var consumer = new EventingBasicConsumer(channel);
+
+            channel.BasicConsume("hello-queue",false,consumer);
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                Thread.Sleep(1500);
+                Console.WriteLine("Gelen mesaj :" + message);
+
+                channel.BasicAck(e.DeliveryTag, false);
+            };
+            Console.ReadLine();
+        }
+        
+        #region Ayrı bir method ile yapılırsa received böyle oluyor.
+        //private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        //{
+        //    throw new NotImplementedException();
+        //} 
+        #endregion
+    }
+}
